0b189d509fc1991c4da0ac47bc1e2f7678451fe8
hatem782 <hatembenechikh100@gmail.com>
2025-02-15 21:47:12 +0100

    push client interface

--- car-controller/app/page.tsx
+++ car-controller/app/page.tsx
@@ -1,101 +1,152 @@
-import Image from "next/image";
+"use client";
+import {
+  ArrowBigDown,
+  ArrowBigUp,
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  Camera,
+  Flashlight,
+  Hand,
+} from "lucide-react";
+import { useEffect, useState } from "react";
+
+const SERVER_URL = "ws://51.38.49.42:6001/";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [ws, setWs] = useState<WebSocket | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const socket = new WebSocket(SERVER_URL);
+    socket.onopen = () => console.log("Connected to WebSocket");
+    socket.onclose = () => console.log("Disconnected from WebSocket");
+    setWs(socket);
+    return () => socket.close();
+  }, []);
+
+  const sendCommand = (command: string) => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send(command);
+      console.log(`Sent: ${command}`);
+    }
+  };
+
+  const Forward = () => {
+    console.log("FORWARD");
+    sendCommand("FORWARD");
+  };
+  const Backward = () => {
+    console.log("BACKWARD");
+    sendCommand("BACKWARD");
+  };
+  const Left = () => {
+    console.log("LEFT");
+    sendCommand("LEFT");
+  };
+  const Right = () => {
+    console.log("RIGHT");
+    sendCommand("RIGHT");
+  };
+  const Stop = () => {
+    console.log("STOP");
+    sendCommand("STOP");
+  };
+  const Light = () => {
+    console.log("LIGHT");
+    sendCommand("LIGHT");
+  };
+
+  const Nothing = () => {};
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="flex-grow w-full flex justify-center items-center">
+        <div className="w-1/4 h-full">
+          <Button Icon={ArrowLeft} onPress={Right} onRelease={Stop} />
+          <Button
+            Icon={Flashlight}
+            color="bg-yellow-500"
+            onPress={Light}
+            onRelease={Nothing}
+          />
+          <Button Icon={ArrowRight} onPress={Left} onRelease={Stop} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <VideoScreen />
+        <div className="w-1/4 h-full">
+          <Button Icon={ArrowUp} onPress={Forward} onRelease={Stop} />
+          <Button
+            Icon={Hand}
+            color="bg-red-500"
+            onPress={Stop}
+            onRelease={Nothing}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Button Icon={ArrowDown} onPress={Backward} onRelease={Stop} />
+        </div>
+      </div>
+
+      {/* <div className="grid grid-cols-3 gap-4">
+        <button className="btn" onClick={() => sendCommand("FORWARD")}>
+          ⬆️
+        </button>
+        <button className="btn" onClick={() => sendCommand("LEFT")}>
+          ⬅️
+        </button>
+        <button className="btn" onClick={() => sendCommand("RIGHT")}>
+          ➡️
+        </button>
+        <button className="btn" onClick={() => sendCommand("BACKWARD")}>
+          ⬇️
+        </button>
+        <button className="btn bg-red-500" onClick={() => sendCommand("STOP")}>
+          ⏹ STOP
+        </button>
+        <button
+          className="btn bg-yellow-500"
+          onClick={() => sendCommand("LIGHT")}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          💡 LIGHT
+        </button>
+      </div> */}
     </div>
   );
 }
+
+const Button = ({
+  Icon,
+  color = "bg-blue-600",
+  onPress = () => {},
+  onRelease = () => {},
+}: {
+  Icon: any;
+  color?: string;
+  onPress?: any;
+  onRelease?: any;
+}) => {
+  return (
+    <div
+      className="w-full h-1/3 flex items-center justify-center"
+      onMouseDown={onPress}
+      onMouseUp={onRelease}
+      onTouchStart={onPress}
+      onTouchEnd={onRelease}
+    >
+      <div
+        className={`w-[90%] h-[90%] ${color} rounded-lg flex items-center justify-center !text-white`}
+      >
+        <Icon className="w-[8vw] h-[8vw] !text-white" />
+      </div>
+    </div>
+  );
+};
+
+const VideoScreen = () => {
+  return (
+    <div className="w-2/4 h-full flex items-center justify-center">
+      <div className="w-[98%] h-[96.5%] bg-blue-600 rounded-lg flex flex-col items-center justify-center">
+        <Camera className="text-white w-[30vw] h-[30vw]" />
+        <h1 className="text-white font-bold text-[5vw]">COMING SOON</h1>
+      </div>
+    </div>
+  );
+};
